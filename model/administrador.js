e2c680f8b25ea1eb4be6433992d985198a5e0c38
Miguel Dufloth <migueldufloth@outlook.com>
2025-04-24 21:37:13 -0300

    Create administrador.js

---
+++ model/administrador.js
@@ -0,0 +1,9 @@
+export class Administrador {
+    constructor(codigo,nome,email,senha,foto){
+        this.codigo = codigo
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.foto = foto
+    }
+}
