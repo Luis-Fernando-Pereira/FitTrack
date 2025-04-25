7e5a1c666c3125778b6b099b0e68548d33c89ce9
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2025-04-24 21:31:16 -0300

    adicionado modelo clietne e alteracao app.js para rotas de administrador

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin/index')
 
 var app = express();
 
@@ -20,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/admin', adminRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- model/cliente.js
+++ model/cliente.js
@@ -1,3 +1,11 @@
 export class Cliente {
-
+    constructor(codigo,nome,email,senha,idade,sexo,peso){
+        this.codigo = codigo
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.idade = idade
+        this.sexo = sexo
+        this.peso = peso
+    }
 }
