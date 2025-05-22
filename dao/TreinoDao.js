23af21175853ff60dc869e3e6db5b1838e74bbd6
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2025-05-22 20:46:14 -0300

    Adicionado função que deleta registro do banco de dados

--- dao/TreinoDao.js
+++ dao/TreinoDao.js
@@ -17,10 +17,10 @@ class TreinoDao{
      * @param {TreinoModel} model retorna um objeto modelo de 
      * treino em caso de sucesso. Caso contrário, retorna false. 
      */
-    async inserir(model){
+    async inserir(treino){
     
         const sql = 'INSERT INTO treino(descricao_tre, titulo_tre) values (?,?);';
-        const { insertid } = await this.conexao.query(sql, model.toInsertArray());
+        const { insertid } = await this.conexao.query(sql, treino.toInsertArray());
         
         await this.conexao.end();
 
@@ -31,5 +31,37 @@ class TreinoDao{
         model.codigo = insertid;
 
         return model;
+    }
+
+    /**
+     * Função que atualiza registro de treino no banco de dados
+     * @param {TreinoModel} treino 
+     */
+    async atualizar(treino){
+        const sql = 'UPDATE treino set descricao_tre = ?, titulo_tre = ? WHERE cod_tre = ?';
+        const { affectedRows } = await this.conexao.query(sql, treino.toUpdateArray());
+
+        await this.conexao.end();
+
+        if(!affectedRows || affectedRows.length < 1){
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Função que deleta um registro de treino no banco de dados
+     * @param {TreinoModel} treino 
+     */
+    async deletar(treino){
+        const sql = 'DELETE FROM treino WERE cod_tre = ?';
+        const { affectedRows } = await this.conexao.query(sql, [treino.codigo]);
+
+        if(!affectedRows || affectedRows.length < 1 || affectedRows.length > 1){
+            return false;
+        }
+
+        return true;
     }
 }
