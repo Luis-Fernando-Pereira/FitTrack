8bcac7365a470ba2d43aaf1f9e74cb8994881f2a
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2025-05-29 17:00:08 -0300

    corrigido rota de admin em app.js

--- routes/admin/treino.js
+++ routes/admin/treino.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const treinoController = require('../../controller/TreinoController');
 
-router.get('/listarTodos',               treinoController.listarTreinos); 
-router.post('/criar',                    treinoController.criarTreino);
-router.post('/adicionarExercicio',       treinoController.adicionarExercicios);
-router.put('/atualizar',                 treinoController.atualizarTreino);
-router.delete('/desvincularExercicio',   treinoController.removerExercicios);
+router.get('/listarTodos',                  treinoController.listarTreinos); 
+router.post('/criar',                       treinoController.criarTreino);
+router.post('/adicionarExercicio/:id',      treinoController.adicionarExercicios);
+router.put('/atualizar/:id',                treinoController.atualizarTreino);
+router.delete('/desvincularExercicio/:id',  treinoController.removerExercicios);
