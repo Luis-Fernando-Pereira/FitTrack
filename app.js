44f9ed80167a3f51f1c3fb6299814b80c477c00e
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2025-06-28 10:22:24 -0300

    Adicionando rota para chamadas a api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 
 var adminRouter = require('./routes/admin');
 var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/javascript', express.static(path.join(__dirname, 'javascript')));
 
 app.use('/',      indexRouter);
 app.use('/admin', adminRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
